7555d9472cf39db46f3775fa3f0c507b9e3b3f21
Parking Master <parkingmaster@email.com>
2021-12-12 20:10:46 -0500

    Create keep.js

---
+++ js/keep.js
@@ -0,0 +1,34 @@
+(function() {
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  const token = getCookie("s") || null;
+  if (token) {
+    function get(url) {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open("GET", url, false);
+      xmlHttp.send(null);
+      return xmlHttp.responseText;
+    }
+    const user = JSON.parse(get("https://simplemailer.loca.lt/data/" + token));
+    self.user = {};
+    self.user.email = atob(user.email);
+    self.user.password = atob(user.password);
+    self.user.username = atob(user.username);
+    self.isLoggedIn = true;
+  } else {
+    self.isLoggedIn = false;
+  }
+})();
